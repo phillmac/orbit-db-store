7f829d3825447fcac02cfc3554e68d200641576c
phillmac <phillmackintosh@gmail.com>
2020-06-07 03:53:55 +0000

    Fix typo

--- src/Replicator.js
+++ src/Replicator.js
@@ -166,7 +166,7 @@ class Replicator extends EventEmitter {
 
     this._fetching[hash] = hash
     this.emit('load.added', entry)
-    consile.log('Emiited load.added')
+    console.log('Emiited load.added')
     this._stats.tasksStarted += 1
 
     const exclude = []
